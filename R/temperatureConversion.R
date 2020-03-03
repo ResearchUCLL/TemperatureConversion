6a3726b8d20256f2a5762a8ef7485d07d1d1a531
r0663029 <sam.janssens@student.ucll.be>
2020-03-03 11:46:33 +0100

    added fahrenheit to celsius

--- R/temperatureConversion.R
+++ R/temperatureConversion.R
@@ -2,6 +2,10 @@ celsius_to_kelvin <- function(temp_C) {
   temp_K <- temp_C + 273.15
   return(temp_K)
 }
+fahrenheit_to_celsius <- function(temp_F) {
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
+}
 fahrenheit_to_kelvin <- function(temp_F) {
   temp_C <- fahrenheit_to_celsius(temp_F)
   temp_K <- celsius_to_kelvin(temp_C)
